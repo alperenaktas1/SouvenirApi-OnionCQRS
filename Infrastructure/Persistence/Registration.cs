723faf6fe0a9a5ef97453f123ea227ca20ae0303
alper <alperenaktasss24@gmail.com>
2024-05-04 17:59:19 +0300

    write repository yapıldı

--- Infrastructure/Persistence/Registration.cs
+++ Infrastructure/Persistence/Registration.cs
@@ -22,6 +22,7 @@ namespace Persistence
             opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
+            services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
         }
     }
 }
