3f2bddab0940a335ea8574eba21678a0522d778e
alper <alperenaktasss24@gmail.com>
2024-05-05 11:21:18 +0300

    automapper geliştirildi

---
+++ Core/SouvenirApi.Application/Interface/AutoMapper/IMappersApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SouvenirApi.Application.Interface.AutoMapper
+{
+    public interface IMappersApp
+    {
+        TDestination Map<TDestination,TSource>(TSource source,string? ignore = null);
+
+        IList<TDestination> Map<TDestination, Tsource>(IList<Tsource> source, string? ignore = null);
+
+        TDestination Map<TDestination>(object source, string? ignore = null);
+        IList<TDestination> Map<TDestination>(IList<object> source, string? ignore = null);
+    }
+}
---
+++ SouvenirApi.Mapper/Registration.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.DependencyInjection;
+using SouvenirApi.Application.Interface.AutoMapper;
+using SouvenirApi.Mapper.AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SouvenirApi.Mapper
+{
+    public static class Registration
+    {
+        public static void AddCustomMapper(this IServiceCollection service)
+        {
+            service.AddSingleton<IMappersApp, AutoMapper.Mapper>();
+        }
+    }
+}
